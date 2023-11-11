bbb3b585c6c4facc563cdb744fa5f1c602fdd279
nileshshekh <63239794+nileshshekh@users.noreply.github.com>
2023-11-11 07:40:12 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://teenpattimaster-apk.in/' + encodeURI(path as string)
+					'https://teenpattimaster-apk.in' + encodeURI(path as string)
 				}`,
 			},
 		};
